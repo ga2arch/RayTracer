b5d9540e441916b0c5376d3e8639bb68ea8ba8ea
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-10 12:39:44 +0100

    added color class

---
+++ RayTracer/Shape.h
@@ -0,0 +1,22 @@
+//
+//  Shape.h
+//  RayTracer
+//
+//  Created by Gabriele Carrettoni on 10/11/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+#ifndef __RayTracer__Shape__
+#define __RayTracer__Shape__
+
+#include <stdio.h>
+#include "Vector.h"
+#include "Ray.h"
+
+class Intersection {
+    
+    Ray ray;
+        
+};
+
+#endif /* defined(__RayTracer__Shape__) */
