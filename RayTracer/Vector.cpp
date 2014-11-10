d2100788576c17f87a7d528d8b31d953774924ed
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-10 12:12:37 +0100

    added dot and cross product

--- RayTracer/Vector.cpp
+++ RayTracer/Vector.cpp
@@ -96,3 +96,19 @@ inline Vector operator *(const float f, const Vector& v1) {
     Vector v(v1.x * f, v1.y * f, v1.z * f);
     return v;
 }
+
+inline float dot(const Vector& v1, const Vector& v2) {
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+}
+
+inline Vector cross(const Vector& v1, const Vector& v2) {
+    Vector v (v1.y * v2.z - v1.z * v2.y,
+              v1.z * v2.x - v1.x * v2.z,
+              v1.x * v2.y - v1.y * v2.x);
+    return v;
+}
+
+
+
+
+
