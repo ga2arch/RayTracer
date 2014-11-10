c3c1c5afbbb4ffa17565f8a6f1dd6fb2e1ce746c
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-10 12:26:07 +0100

    added class Ray, moved inlines into header

--- RayTracer/Vector.cpp
+++ RayTracer/Vector.cpp
@@ -70,43 +70,6 @@ Vector& Vector::operator/=(const float f) {
     return *this;
 }
 
-// Inlines
-
-inline Vector operator +(const Vector& v1, const Vector& v2) {
-    Vector v(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-    return v;
-}
-
-inline Vector operator *(const Vector& v1, const Vector& v2) {
-    Vector v(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-    return v;
-}
-
-inline Vector operator -(const Vector& v1, const Vector& v2) {
-    Vector v(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-    return v;
-}
-
-inline Vector operator *(const Vector& v1, const float f) {
-    Vector v(v1.x * f, v1.y * f, v1.z * f);
-    return v;
-}
-
-inline Vector operator *(const float f, const Vector& v1) {
-    Vector v(v1.x * f, v1.y * f, v1.z * f);
-    return v;
-}
-
-inline float dot(const Vector& v1, const Vector& v2) {
-    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
-}
-
-inline Vector cross(const Vector& v1, const Vector& v2) {
-    Vector v (v1.y * v2.z - v1.z * v2.y,
-              v1.z * v2.x - v1.x * v2.z,
-              v1.x * v2.y - v1.y * v2.x);
-    return v;
-}
 
 
 
