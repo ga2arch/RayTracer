7184480ca683268030ab2c081a4579f81ed98ece
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-10 11:39:05 +0100

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Gabriele Carrettoni on 10/11/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
