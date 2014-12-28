7b550a8c463433e1a7af1ede825d7c6d65080f97
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-12-28 17:57:48 +0100

    added shape methods

--- Shape.cpp
+++ Shape.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "Shape.h"
+
+Shape::Shape(const Transform* o2w, const Transform* w2o, bool ro)
+:object_to_world(o2w), world_to_object(w2o),
+ reverse_orientation(ro), shape_id(next_shape_id++) {}
+
+bool Shape::can_intersect() {
+    return true;
+}
+
+bool Shape::intersect(const Ray &ray, float *thit, float *ray_e) const {
+    return false;
+}
+
+bool Shape::intersect_p(const Ray &ray) const {
+    return false;
+}
--- Shape.h
+++ Shape.h
@@ -10,5 +10,24 @@
 #define __RayTracer__Shape__
 
 #include <stdio.h>
+#include "Transform.h"
+#include "RayTracer.h"
+
+class Shape {
+    
+public:
+    Shape(const Transform* o2w, const Transform* w2o, bool ro);
+    
+    bool can_intersect();
+    bool intersect(const Ray& ray, float* thit, float* ray_e) const;
+    bool intersect_p(const Ray& ray) const;
+    
+    const Transform* object_to_world, *world_to_object;
+    const bool reverse_orientation;
+    
+    const uint32_t shape_id;
+    static uint32_t next_shape_id;
+    
+};
 
 #endif /* defined(__RayTracer__Shape__) */
