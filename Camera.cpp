879d35af65ee37f4594547f6311c3b1cc8aae7f4
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-12-31 18:35:29 +0100

    added class camera

---
+++ Camera.cpp
@@ -0,0 +1,9 @@
+//
+//  Camera.cpp
+//  RayTracer
+//
+//  Created by Gabriele Carrettoni on 31/12/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+#include "Camera.h"
---
+++ Camera.h
@@ -0,0 +1,21 @@
+//
+//  Camera.h
+//  RayTracer
+//
+//  Created by Gabriele Carrettoni on 31/12/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+#ifndef __RayTracer__Camera__
+#define __RayTracer__Camera__
+
+#include <stdio.h>
+
+class Camera {
+    
+public:
+    
+    
+};
+
+#endif /* defined(__RayTracer__Camera__) */
