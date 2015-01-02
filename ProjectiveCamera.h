ccd1dc63f4bb4bcd63ac6e59993f802d2a705bb5
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-01-02 20:10:04 +0100

    added transform multiplication

--- ProjectiveCamera.h
+++ ProjectiveCamera.h
@@ -11,15 +11,20 @@
 
 #include <stdio.h>
 #include "Camera.h"
+#include "Film.h"
 
 class ProjectiveCamera: public Camera {
     
 public:
     ProjectiveCamera(const Transform& c2w, const Transform& proj,
-                     const float screen_window[4], float sopen, float sclose);
+                     const float screen_window[4], float sopen, float sclose,
+                     Film* film);
     
     
-    Transform camera_to_screen; raster_to_camera;
+    Transform camera_to_screen, raster_to_camera;
+    
+protected:
+    Transform screen_to_raster, raster_to_screen;
 };
 
 #endif /* defined(__RayTracer__ProjectiveCamera__) */
