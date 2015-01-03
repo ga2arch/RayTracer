146ee489cc5f3f0d6e85652e1117cf11cf286aa5
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-01-03 20:22:05 +0100

    screen_to_raster

--- ProjectiveCamera.cpp
+++ ProjectiveCamera.cpp
@@ -15,7 +15,11 @@ ProjectiveCamera::ProjectiveCamera(const Transform& c2w, const Transform& proj,
     
     screen_to_raster = Transform::Scale(float(film->x_res),
                                         float(film->y_res),
-                                        float(1.f));
+                                        float(1.f)) *
+        Transform::Scale(1.f / (screen_window[1] - screen_window[0]),
+                         1.f / (screen_window[2] - screen_window[3]),
+                         1.f) *
+        Transform::Translate(Vector(-screen_window[0], -screen_window[3], 0.f));
     
     camera_to_screen = proj;
     raster_to_camera = Transform::Inverse(camera_to_screen) * raster_to_screen;
